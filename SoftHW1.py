b41938e57661621724a80aeaf337dd5bb6179a8f
tanzimelahi <43054153+tanzimelahi@users.noreply.github.com>
2019-09-12 23:16:20 -0400

    Update SoftHW1.py

--- SoftHW1.py
+++ SoftHW1.py
@@ -1,20 +1,22 @@
 import random
-KREWES=dict()
-KREWES['orpheus']=['Emily', 'Kevin','Vishwaa','Erick','ray','Jesse','Tiffany','Amanda',
-                   'Junhee','Jackie','Tyler','Emory','Ivan','Elizabeth','Pratham','Shaw',
-                    'Yaru','Kelvin','Tanzim']
-KREWES['rex']=['William','Joseph','Calvin','Ethan','Moody','Mo','Alex','David','Hannah','Alex','Jiangho','Devin']
-KREWES['endymilon']=['Grace','Nahi','Derek','Manfred','Leila','Ahmed','Wilson','Connor','Brandon','Jackson']
-# to see the size of the list:print 'first list:'+str (len(KREWES['orpheus']))
-#print 'second list:'+str (len(KREWES['rex']))
-#print 'third list:'+str (len(KREWES['endymilon']))
+
+KREWES = {
+
+        'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie ', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+
+        'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+
+        'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
+
+}
+
 
 def chooseStudent():
     x=random.randint(1,3)
     if x==1:
-        print KREWES['orpheus'][random.randint(0,18)]
+        print (KREWES['orpheus'][random.randint(0,18)])
     elif x==2:
-        print KREWES['rex'][random.randint(0,11)]
+        print (KREWES['rex'][random.randint(0,11)])
     else:
-        print KREWES['endymilon'][random.randint(0,9)]
+        print (KREWES['endymilon'][random.randint(0,9)])
 chooseStudent()
